d1a234794fa59dfd242a4396f5f233b6b20d62bd
kafui4k <kalordo7@gmail.com>
2023-09-15 18:13:48 +0000

    add color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/gear-logo-icon-vector-29518208.jpg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text>Nav Bar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
