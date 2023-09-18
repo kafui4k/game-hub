690de9092474e8f60c9d0e2fb346c82d4a022c38
kafui4k <kalordo7@gmail.com>
2023-09-18 13:28:28 +0000

    display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,4 +1,4 @@
-import  { ReactNode } from "react";
+import { ReactNode } from "react";
 import { Box } from "@chakra-ui/react";
 
 interface Props {
@@ -6,9 +6,11 @@ interface Props {
 }
 
 const GameCardContainer = ({ children }: Props) => {
-  return <Box borderRadius={10} overflow="hidden" width="300px">
-    {children}
-  </Box>;
+  return (
+    <Box borderRadius={10} overflow="hidden" width="100%">
+      {children}
+    </Box>
+  );
 };
 
 export default GameCardContainer;
